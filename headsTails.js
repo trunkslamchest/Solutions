93757a97cc5705653c45723eda820d385006fb21
trunkslamchest <trunk9slamchest@gmail.com>
2020-08-14 12:52:21 -0400

    added headsTails.js

---
+++ headsTails.js
@@ -0,0 +1,55 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Problem
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// Write a function that finds all combinations of a coin flip when flipped n times.
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Constraints
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// 1 <= n <= 50
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Pseudo Code
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Solutions
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// var headsTails = function(n){
+//   const arr = ['heads', 'tails'], res = []
+
+//   for (i = 0; i < 2 ** n; i++) {
+//      let temp = []
+//       for (j = 0; j < n; j++) {
+//         if (i & Math.pow(2, j)) temp.push(arr[0])
+//         else temp.push(arr[1])
+//       }
+//     res.push(temp)
+//   }
+
+//   return res
+// }
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+console.log(headsTails(2)) //
+
+
+
+console.log(headsTails(3)) //
+
+
+
+console.log(headsTails(4)) //
+
+
+
+console.log(headsTails(5)) //
+
+
